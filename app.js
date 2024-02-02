909edf262ff3cf905dc314d2d0cfd86a58238301
ammarahm <ammar69aa59@gmail.com>
2024-02-02 12:05:19 +0500

    Javascript learn

---
+++ app.js
@@ -0,0 +1,19 @@
+//Chapter1 & 2
+//Code for  name & nationality// 
+
+var myName1 = "Ammar ";
+var nationality1 = "Pakistan";
+alert("My name is " + myName1 + ". I am from " + nationality1 + " .");
+//Javascript code to tell browser that I am a SMIT student
+
+var myName = "Ammar";
+var lastName = "Ahmed";
+var nationality = "Pakistani";
+var work = "I am a student in SMIT";
+var course = "Web & Moblie app development";
+var id = 227780;
+var instructor = "Sir Rizwan Bhatti";
+var batch = 11;
+var SMIT = "Saylani Mass IT Training";
+alert("Welcome " + myName + " " + lastName + " .My natonality is " + nationality + " " + work + " . I am learning " + course + " . My roll number there is " + " " + id + " . Our course instructor is " + instructor + " . My batch is " + batch + ". SMIT stands for " + SMIT);
+
