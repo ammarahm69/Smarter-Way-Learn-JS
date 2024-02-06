2147e245942f72e9a7368c34fbd17671e6621f3b
ammarahm <ammar69aa59@gmail.com>
2024-02-06 11:47:33 +0500

    js practice

--- app.js
+++ app.js
@@ -1,19 +1,69 @@
 //Chapter1 & 2
 //Code for  name & nationality// 
 
-var myName1 = "Ammar ";
-var nationality1 = "Pakistan";
-alert("My name is " + myName1 + ". I am from " + nationality1 + " .");
+// var myName1 = "Ammar ";
+// var nationality1 = "Pakistan";
+// alert("My name is " + myName1 + ". I am from " + nationality1 + " .");
 //Javascript code to tell browser that I am a SMIT student
 
-var myName = "Ammar";
-var lastName = "Ahmed";
-var nationality = "Pakistani";
-var work = "I am a student in SMIT";
-var course = "Web & Moblie app development";
-var id = 227780;
-var instructor = "Sir Rizwan Bhatti";
-var batch = 11;
-var SMIT = "Saylani Mass IT Training";
-alert("Welcome " + myName + " " + lastName + " .My natonality is " + nationality + " " + work + " . I am learning " + course + " . My roll number there is " + " " + id + " . Our course instructor is " + instructor + " . My batch is " + batch + ". SMIT stands for " + SMIT);
+// var myName = "Ammar";
+// var lastName = "Ahmed";
+// var nationality = "Pakistani";
+// var work = "I am a student in SMIT";
+// var course = "Web & Moblie app development";
+// var id = 227780;
+// var instructor = "Sir Rizwan Bhatti";
+// var batch = 11;
+// var SMIT = "Saylani Mass IT Training";
+// alert("Welcome " + myName + " " + lastName + " .My natonality is " + nationality + " " + work + " . I am learning " + course + " . My roll number there is " + " " + id + " . Our course instructor is " + instructor + " . My batch is " + batch + ". SMIT stands for " + SMIT);
 
+
+
+// Chap 2
+// alert(300);
+// var weight = 150;
+// var weight1 = 500;
+// alert(weight + weight1);
+
+// var originalNum = 80;
+// var localNum = 40;
+// var originalNum = originalNum + localNum;
+// console.log(originalNum + " is the total amount for your work");
+
+// // chap3
+// var userName = "Ammar"
+// var likeEats = "biryani"
+// console.log("User name is " + userName + " he like to eat " + likeEats);
+
+// var num = 200;
+// var popNum = 100;
+// var popNum = popNum + num;
+// console.log(popNum);
+
+
+// var numOrgin = 13;
+// var minus = 10;
+// var finalResult = minus - numOrgin;
+// console.log(finalResult);
+
+// var remainder = 25 % 3;
+// console.log(remainder);
+
+
+// var whatsLeftOver = 9 % 3;
+// alert("The remainder is " + whatsLeftOver);
+
+// a++ (post-increment)
+// ++a (pre-increment)
+// var Input = 90;
+// var newNum = ++Input;
+// alert(Input);
+// // console.log(Input);
+
+
+var question = "Remainder ko plus krna ha";
+alert(question);
+var num = 29;
+var num1 = 5;
+var newNum = (num % num1) * 2 / 20 ;
+console.log(newNum);
